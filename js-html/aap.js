1fefa4a98741444f8a63e9bbbeb5816ae00a6384
Samaha Dev <samaha.ayesha@gmail.com>
2024-03-19 22:04:09 +0500

    paractice-js

---
+++ js-html/aap.js
@@ -0,0 +1,34 @@
+// console.log("hellow world");
+// let radius=prompt("please enter radius of a circle");
+// const PI=3.415;
+// let area=PI*(radius**2);
+// alert(area)
+// let height=prompt("please enter your height in inches")
+// let weight=prompt("please enter your weight in puonds")
+// let BMI=(weight*703)/(height**2);
+// alert(BMI)
+// let year=prompt("enter your date of birth");
+// let age=(2024-year);
+// alert(age);
+// let a=10
+// let b=20
+// let c=a;
+// a=b; b=c;
+// alert(b);
+// alert(a);
+// let value=prompt("enter your value");
+// let number=prompt("enter your number");
+// let sum=parseInt(value)+parseInt(number);
+// alert(sum);
+//  let age1=prompt("enter the age of first student");
+//  let age2=prompt("enter the age of second student");
+//  let age3=prompt("enter the age of second student");
+//  let age4=prompt("enter the age of fourth student");
+//  let age5=prompt("enter the age of fifth student");
+//  let age6=prompt("enter the age of sixth student");
+//  let age7=prompt("enter the age of seven student");
+//  let age8=prompt("enter te age of eight student");
+//  let age9=prompt("enter the age of nine student");
+//  let age10=prompt("enter the age of ten student");
+//  let average=(parseInt(age1)+parseInt(age2)+parseInt(age3)+parseInt(age4)+parseInt(age5)+parseInt(age6)+parseInt(age7)+parseInt(age8)+parseInt(age9)+parseInt(age10))/10;
+//  alert(parseInt(average));
