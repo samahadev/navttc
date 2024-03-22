6b1e310fd6350506b299d194ea58a1081360a78f
Samaha Dev <samaha.ayesha@gmail.com>
2024-03-22 15:41:45 +0500

    new file for assigment

--- paractice-file/js-html/asg.js
+++ paractice-file/js-html/asg.js
@@ -1,7 +1,23 @@
-let tweet = prompt("Enter your tweet: ");
-let tweetLength = tweet.length;
-let result =
-  tweetLength <= 140
-    ? `Your ${tweetLength} char tweet will work!`
-    : `Your ${tweetLength} char tweet is ${tweetLength - 140} chars too long`;
-console.log(result);
+// let tweet = prompt("Enter your tweet: ");
+// let tweetLength = tweet.length;
+// let result =
+//   tweetLength <= 140
+//     ? `Your ${tweetLength} char tweet will work!`
+//     : `Your ${tweetLength} char tweet is ${tweetLength - 140} chars too long`;
+// console.log(result);
+// let age=prompt("please enter your age")
+// if(age<18){
+//   console.log("sorry,you are too young to drive this car.powering off")
+// }
+// else if(age==18){
+//   console.log("congratulation on your first year of driving.Enjoy the ride!")
+// }
+// else{
+//   console.log("powering on .Enjoy the ride!")
+// }
+// console.log("welcome to our shop");
+// let itemname=prompt("please enter the name of the item tat you are buying");
+// let itemprice=prompt("plese enter the price of the item");
+// let itemquantity=prompt("please enter the quantity of te item");
+// let subtotal=itemquantity*itemprice;
+// console.log(subtotal);
