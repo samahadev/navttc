a515a361f6bd61ed1e083e25685a23d60f3f022c
Samaha Dev <samaha.ayesha@gmail.com>
2024-03-26 22:19:40 +0500

    practice

--- paractice-file/js-html/assigment-2.js
+++ paractice-file/js-html/assigment-2.js
@@ -1,36 +1,44 @@
-// let monthnumber = prompt("please enter the number of a month");
+// // // let monthnumber = prompt("please enter the number of a month");
 
-// if (monthnumber == 1) {
-//   console.log("january has 31 days");
-// } else if (monthnumber == 2) {
-//   console.log("february has 28 days");
-// } else if (monthnumber == 3) {
-//   console.log("march has 31 days");
-// } else if (monthnumber == 4) {
-//   console.log("april has 30 days");
-// } else if (monthnumber == 5) {
-//   console.log("may has 31 days");
-// } else if (monthnumber == 6) {
-//   console.log("june has 30 days");
-// } else if (monthnumber == 7) {
-//   console.log("july has 31 days");
-// } else if (monthnumber == 8) {
-//   console.log("august has 31 days");
-// } else if (monthnumber == 9) {
-//   console.log("september has 30 days");
-// } else if (monthnumber == 10) {
-//   console.log("october has 31 days");
-// } else if (monthnumber == 11) {
-//   console.log("november has 30 days");
-// } else if (monthnumber == 12) {
-//   console.log("december has 31 days");
-// } else {
-//   console.log("A year consists of 12 month please enter number between 1 to 12");
-// }
-// else{
-//   document.body.innerHTML =
-//     "you enter a incorrect pin lease enter a number between 1 to 12";
-// }
-let name=prompt("please enter your name");
-let 
-let Ayesha=name[Ayesha].toupperCase()+name.slice(1).toLowerCase();
+// // // if (monthnumber == 1) {
+// // //   console.log("january has 31 days");
+// // // } else if (monthnumber == 2) {
+// // //   console.log("february has 28 days");
+// // // } else if (monthnumber == 3) {
+// // //   console.log("march has 31 days");
+// // // } else if (monthnumber == 4) {
+// // //   console.log("april has 30 days");
+// // // } else if (monthnumber == 5) {
+// // //   console.log("may has 31 days");
+// // // } else if (monthnumber == 6) {
+// // //   console.log("june has 30 days");
+// // // } else if (monthnumber == 7) {
+// // //   console.log("july has 31 days");
+// // // } else if (monthnumber == 8) {
+// // //   console.log("august has 31 days");
+// // // } else if (monthnumber == 9) {
+// // //   console.log("september has 30 days");
+// // // } else if (monthnumber == 10) {
+// // //   console.log("october has 31 days");
+// // // } else if (monthnumber == 11) {
+// // //   console.log("november has 30 days");
+// // // } else if (monthnumber == 12) {
+// // //   console.log("december has 31 days");
+// // // } else {
+// // //   console.log("A year consists of 12 month please enter number between 1 to 12");
+// // // }
+// // // else{
+// // //   document.body.innerHTML =
+// // //     "you enter a incorrect pin lease enter a number between 1 to 12";
+// // // }
+// let name = prompt("please enter your name");
+// // // let
+// let Ayesha = name[0].toUpperCase() + name.slice(1).toLowerCase();
+
+// let a = 0;
+// let b = 0;
+// let c = 0;
+
+// // console.log((++a + b ) * ++c);
+// console.log((++a + ++b) ** ++c);
+
