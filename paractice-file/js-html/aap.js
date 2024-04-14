5964558086ba60dd2097255b97bdd43bb72c32e0
Samaha Dev <samaha.ayesha@gmail.com>
2024-04-14 15:09:27 +0500

    practice

--- paractice-file/js-html/aap.js
+++ paractice-file/js-html/aap.js
@@ -72,7 +72,7 @@
 
 
 
-// // assigment
+// // assigment 1
 // function lifePhase(age) {
 // if (age>0 && age<= 3) {
 //     return "baby";
@@ -196,10 +196,129 @@
 //     console.log(`${capital}${remaining}`)
 // }
 
+// // assigment 9
+// // PART 1
+// const word = "supercalifragilisticexpialidocious";
 
+// // For loop
+// for (let i = 0; i < word.length; i++) {
+//     console.log(word[i]);
+// }
 
+// // While loop
+// let index = 0;
+// while (index < word.length) {
+//     console.log(word[index]);
+//     index++;
+// }
 
+// // PART 2
+// // While loop for even numbers from 100 to 140
+// let number = 100;
+// while (number <= 140) {
+//     if (number % 2 === 0) {
+//         console.log(number);
+//     }
+//     number++;
+// }
 
+// // For loop with range
+// for (let num = 100; num <= 140; num++) {
+//     if (num % 2 === 0) {
+//         console.log(num);
+//     }
+// // PART 3
+// let passphrase = "";
+// while (passphrase !== "sillygoose") {
+//     passphrase = prompt("Please enter the passphrase 'sillygoose':");
+// }
+// // assigment 10
+// // PART 1
+// function contains_pickle(...args) {
+//     return args.includes("pickle");
+// }
+
+// // Test cases
+// console.log(contains_pickle("red", 45, "pickle", []));  // true
+// console.log(contains_pickle(1, 2, "blue"));  // false
+
+// // PART 2
+// function count_fails(...args) {
+//     return args.filter(score => score <= 50).length;
+// }
+
+// // Test cases
+// console.log(count_fails(99, 48, 79, 36));  // 2
+// console.log(count_fails(85, 78, 91));  // 0
+// console.log(count_fails(50, 41, 47, 74, 76, 81));  // 3
+
+// // PART 3
+// function get_top_students(...args) {
+//     return args.filter(([name, score]) => score >= 90).map(([name]) => name);
+// }
+
+// // Test cases
+// console.log(get_top_students(["tim", 91], ["stacy", 83], ["carlos", 97], ["jim", 69]));  // ['tim', 'carlos']
+// console.log(get_top_students(["colt", 61], ["elton", 76]));  // []
+// console.log(get_top_students(["kitty", 80], ["blue", 95], ["toad", 91]));  // ['blue', 'toad']
+
+// // assigment 11
+// function sumArray(arr) {
+//   // Initialize a variable to store the sum
+//   let sum = 0;
+
+//   // Loop through each element in the array
+//   for (let num of arr) {
+//     // Add the current element to the sum
+//     sum += num;
+//   }
+
+//   // Return the final sum
+//   return sum;
+// }
+
+// // Example usage:
+// console.log(sumArray([1, 2, 3]));       // Output: 6
+// console.log(sumArray([2, 2, 2, 2]));     // Output: 8
+// console.log(sumArray([50, 50, 1]));     // Output: 101
+
+// // assigment 12
+// function reverseArray(arr) {
+//   // Create a new empty array to store the reversed elements
+//   const reversedArr = [];
+
+//   // Loop through the original array in reverse order (starting from the last element)
+//   for (let i = arr.length - 1; i >= 0; i--) {
+//     // Push each element from the original array to the new reversed array
+//     reversedArr.push(arr[i]);
+//   }
+
+//   // Return the new reversed array
+//   return reversedArr;
+// }
+
+// // Example usage:
+// const sentences = ['sense', 'make', 'all', 'will', 'this'];
+// const reversedSentences = reverseArray(sentences);
+
+// console.log(sentences);  // Output: ['sense', 'make', 'all', 'will', 'this'] (original array remains unchanged)
+// console.log(reversedSentences);  // Output: ['this', 'will', 'all', 'make', 'sense'] (reversed array)
+
+// // assigment 13
+// function greetAlien(alienNames) {
+//   // Loop through each alien name in the array
+//   for (const alienName of alienNames) {
+//     // Construct the greeting message
+//     const greeting = `Oh powerful ${alienName}, we humans offer our unconditional surrender!`;
+
+//     // Print the greeting message to the console
+//     console.log(greeting);
+//   }
+// }
+
+// // Example usage:
+// const alien = ["blorgous", "glamyx", "wegord", "spaceking"];
+// greetAlien(alien);
 
 
 
